14f56a38960868d261cec23310b70f8894d36641
Shaikat <shaikat700@gmail.com>
2025-02-06 14:16:18 +0600

    Variable Declare

---
+++ practice.js
@@ -0,0 +1,20 @@
+/* let age = 18;
+
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+} */
+
+var weight=38;
+var money=200;
+var year=1999;
+
+var sh;
+console.log(money);
+console.log(year);
+
+
+var age = 17;
+age = 35;
+console.log(age);
